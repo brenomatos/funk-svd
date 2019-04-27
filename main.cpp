cf709d676053085a10a579a69aab18b211536c90
breno <breno.ms1997@gmail.com>
2019-04-26 22:38:50 -0300

    reading input files

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 class Matrix {
 private:
 
-  // add getters and setters (cleaner code)     
+  // add getters and setters (cleaner code)
 public:
   int **m;
   int col;
   int row;
   Matrix (int col, int row);
   ~Matrix ();
+  void print_matrix();
 };
 
 Matrix::Matrix(int col, int row){
@@ -30,20 +30,39 @@ Matrix::~Matrix(){
   free(m);
 }
 
+void Matrix::print_matrix(){
+  for (int i = 0; i < this->col; i++) {
+    for (int j = 0; j < this->row; j++) {
+      cout << this->m[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
 int main(int argc, char const *argv[]) {
+  ifstream ratings, targets;
+  ratings.open(argv[1]);
+  targets.open(argv[2]);
+  string line; // needed to read line by line from each file
+
+  if (targets.is_open())
+  {
+    while ( getline (targets,line) )
+    {
+      cout << line << '\n';
+    }
+  }
+
   Matrix matriz = Matrix(3,3);
   for (int i = 0; i < matriz.col; i++) {
     for (int j = 0; j < matriz.row; j++) {
       matriz.m[i][j] = i+j;
     }
   }
+  matriz.print_matrix();
 
-  for (int i = 0; i < matriz.col; i++) {
-    for (int j = 0; j < matriz.row; j++) {
-      cout << matriz.m[i][j] << " ";
-    }
-    cout << endl;
-  }
 
+  ratings.close();
+  targets.close();
   return 0;
 }
