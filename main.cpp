0ede1fe56c4bb671de5348eaff1e84b8fa482e4c
breno <breno.ms1997@gmail.com>
2019-05-04 00:49:54 -0300

    tuned parameters

--- main.cpp
+++ main.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 class Matrix;
 
-// params 50,0.005,0.8 epochs 73
-// como checar se uma chave existe de forma eficiente?
+// params 37,0.005,0.8 epochs 49
+// RMSE DO MELHOR RESULTADO (EM TREINO): 1.38441
 int main(int argc, char const *argv[]) {
   (void)argc;
   ifstream input_ratings, input_targets;
   input_ratings.open(argv[1]);
   input_targets.open(argv[2]);
 
-  int k=50; // # of factors
+  int k=37; // # of factors
   double learning_rate=0.005;
   double reg=0.8;
-  int epochs = 73; //testra 23
+  int epochs = 49; //testra 23
 
   Svd svd = Svd(k,learning_rate,reg,epochs,&input_ratings);
   svd.train_model();
