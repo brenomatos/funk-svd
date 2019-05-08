10c36f45e7302632c2ab94ed6aa9cc67cbb84289
BrenoMatos <brenomatos@grande.grad.dcc.ufmg.br>
2019-05-07 21:14:33 -0300

    added final version

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class Matrix;
 
-// params 23,0.005 epochs 80
+// params 300,0.005 epochs 50
 // RMSE DO MELHOR RESULTADO (EM TREINO): 1.38441
 int main(int argc, char const *argv[]) {
   (void)argc;
@@ -13,9 +13,9 @@ int main(int argc, char const *argv[]) {
   input_ratings.open(argv[1]);
   input_targets.open(argv[2]);
 
-  int k=23; // # of factors
+  int k=300; // # of factors
   double learning_rate=0.005;
-  int epochs = 80; //testra 23
+  int epochs = 50; //testra 23
 
   Svd svd = Svd(k,learning_rate,epochs,&input_ratings, &input_targets);
   svd.train_model();
